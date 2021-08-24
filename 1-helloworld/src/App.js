ffbcf876b200e527e99f9cd0349a2f41036f84a3
HunXmoN <maysaputra2305@gmail.com>
2021-08-25 06:36:32 +0800

    3

--- 1-helloworld/src/App.js
+++ 1-helloworld/src/App.js
@@ -1,30 +1,56 @@
 import React, { Component } from 'react'
 // https://medium.com/@eshwaren/enable-emmet-support-for-jsx-in-visual-studio-code-react-f1f5dfe8809c
 class App extends Component {
-
+    constructor(){
+        super()
+        this.state = {
+            firstname : '',
+            lastname : '',
+            name : ''
+        }
+    }
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+    submitHandler = (e) => {
+        e.preventDefault()
+        this.setState({
+            name:`${this.state.firstname} ${this.state.lastname}`
+        })
+        // console.log(this.state)
+        this.state.firstname = ''
+        this.state.lastname = ''
+    }
     render() {
+        const {name} = this.state
         return ( 
             <div className = "p-5" >
                 <div className = "container" >
                     <div className = "row" >
-                        <div className = "col-md-6" >
+                        <div className = "col-md-4" >
                             <div className = "card" >
+
                                 <div className = "card-header" >
                                     Learn React 
                                 </div> 
                                 <div className = "card-body" >
-                                    <form>
+                                    <form onSubmit={this.submitHandler}>
                                         <div className= "mb-4">
                                             <label htmlFor= "firstname" className = "form-label">First Name</label>
-                                            <input type="text" name="firstname" id = "firstname" className="form-control"></input>
+                                            <input type="text" value={this.state.firstname} onChange={this.changeHandler} name="firstname" id = "firstname" className="form-control"></input>
                                         </div>
                                         <div className= "mb-4">
-                                            <label htmlFor= "laastname" className = "form-label">Last Name</label>
-                                            <input type="text" name="lasttname" id = "lasttname" className="form-control"></input>
+                                            <label htmlFor= "lastname" className = "form-label">Last Name</label>
+                                            <input type="text" value={this.state.lastname} onChange={this.changeHandler} name="lastname" id = "lastname" className="form-control"></input>
                                         </div>
                                         <button type="submit" className="btn btn-primary btn-block">Lihat</button>
                                     </form> 
                                 </div> 
+                                <div className="card-footer">
+                                    My Name is <b>{name ? name : '. . .'}</b> 
+                                </div>
                             </div>
                         </div> 
                     </div> 
